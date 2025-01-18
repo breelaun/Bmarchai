5ae3685be317f5cdd65bc190e2184656433b45cc
breelaun <circuitstash@gmail.com>
2025-01-18 13:22:16 +0800

    Update CalorieBurner.tsx

--- src/components/home/CalorieBurner.tsx
+++ src/components/home/CalorieBurner.tsx
@@ -94,8 +94,8 @@ const CalorieBurner = () => {
   };
 
   return (
-    <div className="w-full max-w-5xl mx-auto p-7">
-      <Card className="p-7">
+    <div className="w-full max-w-4xl mx-auto p-6">
+      <Card className="p-6">
         <h2 className="text-2xl font-heading font-bold text-center mb-6">
           How To Burn {calories || '___'} Calories
         </h2>
