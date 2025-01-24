22cc6ac217ece97040d3f9ccaea29af84d94bcfe
breelaun <circuitstash@gmail.com>
2025-01-24 17:40:39 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import "./index.css";
+
+Sentry.init({
+ dsn: "https://43b96a275e539584834b17b2d3d42192@o4508694101753856.ingest.us.sentry.io/4508694117089280",
+ integrations: [
+   Sentry.browserTracingIntegration(),
+   Sentry.replayIntegration(),
+ ],
+ tracesSampleRate: 1.0,
+ tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+ replaysSessionSampleRate: 0.1,
+ replaysOnErrorSampleRate: 1.0,
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </React.StrictMode>
+ <React.StrictMode>
+   <Sentry.ErrorBoundary fallback={({ error }) => (
+     <div>An error occurred: {error.message}</div>
+   )}>
+     <QueryClientProvider client={queryClient}>
+       <BrowserRouter>
+         <App />
+       </BrowserRouter>
+     </QueryClientProvider>
+   </Sentry.ErrorBoundary>
+ </React.StrictMode>
 );
