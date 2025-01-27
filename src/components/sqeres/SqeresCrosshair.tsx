b4a740af97c2099d9fc20f9c0743e41dfce56067
breelaun <circuitstash@gmail.com>
2025-01-27 21:32:12 +0800

    Create SqeresCrosshair.tsx

---
+++ src/components/sqeres/SqeresCrosshair.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { CrosshairProps } from './types';
+
+export const SqeresCrosshair: React.FC<CrosshairProps> = ({
+  color = 'white',
+  containerRef = null,
+}) => {
+  // Your existing Crosshair component code with TypeScript types
+  // ... (Previous crosshair code with added type safety)
+};
