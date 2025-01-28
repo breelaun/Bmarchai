696efe69d560a2434163a8a51e79302bb929c867
breelaun <circuitstash@gmail.com>
2025-01-28 19:37:53 +0800

    Create PuzzlePage.tsx

---
+++ src/pages/PuzzlePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WordSearch from "@/components/puzzle/WordSearch";
+
+const PuzzlePage = () => {
+  return (
+    <div className="container mx-auto mt-24">
+      <WordSearch />
+    </div>
+  );
+};
+
+export default PuzzlePage;
