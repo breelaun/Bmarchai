f457fae2fe0807dc3b069a9b1e6410407a518fe3
breelaun <circuitstash@gmail.com>
2025-01-29 11:33:21 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from './App.tsx'
-import './index.css'
+import App from "./App";
+import "./index.css";
 
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
-const root = document.getElementById('root')
-if (!root) throw new Error('Root element not found')
-
-createRoot(root).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
